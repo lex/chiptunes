eac7876098e285c17915f7c605d112fcabaa3fcb
lex <lex@users.noreply.github.com>
2018-03-13 12:47:47 +0200

    play a hello world song

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,43 +4,28 @@
     <h2>Essential Links</h2>
     <ul>
       <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
+        <a href="https://vuejs.org" target="_blank">
           Core Docs
         </a>
       </li>
       <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
+        <a href="https://forum.vuejs.org" target="_blank">
           Forum
         </a>
       </li>
       <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
+        <a href="https://chat.vuejs.org" target="_blank">
           Community Chat
         </a>
       </li>
       <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
+        <a href="https://twitter.com/vuejs" target="_blank">
           Twitter
         </a>
       </li>
       <br>
       <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
           Docs for This Template
         </a>
       </li>
@@ -48,34 +33,22 @@
     <h2>Ecosystem</h2>
     <ul>
       <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://router.vuejs.org/" target="_blank">
           vue-router
         </a>
       </li>
       <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://vuex.vuejs.org/" target="_blank">
           vuex
         </a>
       </li>
       <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
+        <a href="http://vue-loader.vuejs.org/" target="_blank">
           vue-loader
         </a>
       </li>
       <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
           awesome-vue
         </a>
       </li>
@@ -84,6 +57,8 @@
 </template>
 
 <script>
+import Tone from 'tone';
+
 export default {
   name: 'HelloWorld',
   data() {
@@ -91,12 +66,51 @@ export default {
       msg: 'Welcome to Your Vue.js App',
     };
   },
+  methods: {
+    playHelloWorldSong() {
+      const synth = new Tone.Synth().toMaster();
+
+      const notes = [
+        { note: 'C4', length: '4n' },
+        { note: 'C4', length: '4n' },
+        { note: 'C4', length: '4n' },
+        { note: 'E4', length: '4n' },
+        { note: 'D4', length: '4n' },
+        { note: 'D4', length: '4n' },
+        { note: 'D4', length: '4n' },
+        { note: 'F4', length: '4n' },
+        { note: 'E4', length: '4n' },
+        { note: 'E4', length: '4n' },
+        { note: 'D4', length: '4n' },
+        { note: 'D4', length: '4n' },
+        { note: 'C4', length: '2n' },
+        { note: null, length: null },
+        { note: null, length: null },
+      ];
+
+      // eslint-disable-next-line
+      const loop = new Tone.Sequence(
+        (time, note) => {
+          if (!note.note) return;
+          synth.triggerAttackRelease(note.note, note.length);
+        },
+        notes,
+        '4n',
+      ).start();
+
+      Tone.Transport.start();
+    },
+  },
+  created() {
+    this.playHelloWorldSong();
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
